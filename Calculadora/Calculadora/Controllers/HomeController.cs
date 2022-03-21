4e4261f2cab627740c69d1f7b8ab40b01b03674a
RubenPPP <aluno20966@ipt.pt>
2022-03-21 17:12:53 +0000

    v.0.3 - Processamento dos algarismos

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -13,8 +13,51 @@ namespace Calculadora.Controllers
             _logger = logger;
         }
 
+        [HttpGet] // Por predefinição os pedidos http são get
         public IActionResult Index()
         {
+            // Inicializar os dados para a calculadora
+            ViewBag.Visor = "0";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string botao, string visor)
+        {
+            // Vamos decidir a ação a realizar com o valor do botão
+            switch (botao)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    // Pressionou um algarismo
+                    if (visor == "0")
+                        visor = botao;
+                    else
+                        visor += botao;
+                    break;
+                case ",":
+                    // Pressionou a vírgula
+                    if (!visor.Contains(','))
+                        visor += botao;
+                    break;
+                case "+/-":
+                    // Pressinou +/-
+                    if (visor.StartsWith('-'))
+                        visor = visor[1..];
+                    else
+                        visor = '-' + visor;
+                    break;
+            }
+            // Preparar os dados a serem enviados para a View
+            ViewBag.Visor = visor;
             return View();
         }
 
